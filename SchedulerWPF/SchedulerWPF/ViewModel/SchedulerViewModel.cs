a2b96166c2a10b70aacf8f727de85da342a08f16
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-20 17:48:13 +0530

    WPF-56496 - Recurring appointment sample added.

---
+++ SchedulerWPF/SchedulerWPF/ViewModel/SchedulerViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Syncfusion.UI.Xaml.Scheduler;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Media;
+
+namespace WpfScheduler
+{
+    public class SchedulerViewModel
+    {
+        public ScheduleAppointmentCollection scheduleAppointmentCollection { get; set; } = new ScheduleAppointmentCollection();
+        public SchedulerViewModel()
+        {
+            var scheduleAppointment = new ScheduleAppointment
+            {
+                Id = 1,
+                Subject = "Daily scrum meeting",
+                StartTime = new DateTime(2020, 12, 13, 11, 0, 0),
+                EndTime = new DateTime(2020, 12, 13, 12, 0, 0),
+                AppointmentBackground = new SolidColorBrush(Colors.LimeGreen),
+                Foreground = new SolidColorBrush(Colors.White),
+                RecurrenceRule = "FREQ=DAILY;INTERVAL=1;COUNT=10"
+            };
+            scheduleAppointmentCollection.Add(scheduleAppointment);
+
+            DateTime exceptionDate = scheduleAppointment.StartTime.AddDays(3).Date;
+            scheduleAppointment.RecurrenceExceptionDates = new ObservableCollection<DateTime>()
+            {
+              exceptionDate,
+            };
+        }
+    }
+}
